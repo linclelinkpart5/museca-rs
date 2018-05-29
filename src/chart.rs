88e02c3805d266418b3e6bad46e9264b4505008f
Mark LeMoine <thatsgobbles@gmail.com>
2018-05-29 14:03:35 -0700

    Fleshing out Chart

--- src/chart.rs
+++ src/chart.rs
@@ -1,20 +1,9 @@
 use types::{BPM, Offset, TimeSig};
 use event::Event;
 
-mod builder {
-    use event::Event;
-
-    pub struct ChartBuilder {
-        events: Vec<Event>,
-        offset: u32,
-    }
-}
-
-use self::builder::ChartBuilder;
-
 pub struct Chart {
     events: Vec<Event>,
-    offset: u32,
-    start_bpm: u16,
-    start_ts: (u8, u8),
+    offset: Offset,
+    start_bpm: BPM,
+    start_ts: TimeSig,
 }
